b9abdbe2306e3e342c15216c8355d1e043355545
Andrea Freeman <afreeman5566@gmail.com>
2021-09-24 19:01:54 -0500

    commit tree 9d4c665e22727f36c88d0b56619f1ab58adfc0ce

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun()
+
+//
+
+let mondayWork = function(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+mondayWork()
+
+// wrapAdjective
+
+// function wrapAdjective(adjective = "*"){
+//     const encouragingPromptFunction = function(paramater="special"){
+//         return `You are ${parameter}`
+//     }
+// }
+// wrapAdjective()
+
+function wrapAdjective(mark = "*"){
+    return function encouragingPrompt(word = "special"){
+      return `You are ${mark}${word}${mark}!`
+    }
+}
+wrapAdjective("a hard worker")
